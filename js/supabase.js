dd343ceed2de2ec6aca3e71a1b8bd0a01e7915e4
adoracionCDI <595992633899g@gmail.com>
2026-01-27 20:38:58 -0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,5 @@
+// Conexión a Supabase (la que vos ya tenías)
+const supabase = window.supabase.createClient(
+  'https://atmflikzjdhwnssjsxhn.supabase.co',
+  'sb_publishable_Zzdtdqy9KNl6wqy49JJehg_nxPcGyfF'
+);
