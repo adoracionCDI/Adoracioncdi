83da0fadcb297252b6edb1a8d59fcf3c7d3450c7
adoracionCDI <595992633899g@gmail.com>
2026-01-26 22:38:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 const SUPABASE_URL = "https://atmflikzjdhwnssjsxhn.supabase.co";
 const SUPABASE_KEY = "sb_publishable_Zzdtdqy9KNl6wqy49JJehg_nxPcGyfF";
 
-// CREAR CONEXIÓN
-const supabase = window.supabase.createClient(
+// CREAR CLIENTE SUPABASE (UNA SOLA VEZ)
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
 
-// PRUEBA DE CONEXIÓN
-console.log("Supabase conectado:", supabase);
+// PRUEBA
+console.log("Supabase conectado:", supabaseClient);
